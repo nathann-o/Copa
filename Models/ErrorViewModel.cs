293423c826890a1eedda78924b5116b4ce11b3da
nathann <nathann_oliveira@hotmail.com.br>
2020-10-14 15:25:15 -0300

    Primeiro envio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Copa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
