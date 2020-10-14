432abd34c16515c1b889fe9b2e230a2bddbd3a4b
nathann <nathann_oliveira@hotmail.com.br>
2020-10-14 16:53:21 -0300

    Criação do controller

--- Controllers/SelecoesController1.cs
+++ Controllers/SelecoesController1.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Copa.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Copa.Controllers
 {
     public class SelecoesController1 : Controller
     {
-        public IActionResult Index()
+        private readonly Contexto _contexto;
+
+        public SelecoesController1(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _contexto.Selecoes.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult CriarSelecao()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarSelecao(Selecao selecao)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Add(selecao);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else return View(selecao);
         }
     }
 }
